8b47e5c3360b032d3cdca8f67fdc2065303a0196
Sadanand09 <sadanandgurude9@gmail.com>
2025-09-25 17:29:47 +0530

    ref: Custom hook

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 export const TMDB_CONFIG = {
   BASE_URL: "https://api.themoviedb.org/3",
   API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
-  header: {
+  headers: {
     accept: "application/json",
     Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
   },
